7bc416b09e7e717910ec262241506a1413c8cb2e
Emorales <emanuelmoralescarvajal@gmail.com>
2026-08-10 23:00:48 -0600

    prueba1

---
+++ EscuelaCopalchi.UI/Controllers/InventarioCocinaController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EscuelaCopalchi.UI.Controllers
+{
+    public class InventarioCocinaController : Controller
+    {
+        // GET: InventarioCocina
+        public ActionResult Index() {return View();}
+        public ActionResult Create() {return View();}
+    }
+}
