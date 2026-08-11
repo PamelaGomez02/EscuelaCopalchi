0d0cf9674d99c0fd60c9a84d61588ee54a1c0c02
PamelaGomez02 <114601357+PamelaGomez02@users.noreply.github.com>
2026-08-10 20:27:29 -0600

    Creacion de proyecto, vistas de usuario, index, monitoreo

---
+++ EscuelaCopalchi.UI/Controllers/UsuariosController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EscuelaCopalchi.UI.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public ActionResult Index() => View();
+        public ActionResult Create() => View();
+        public ActionResult Edit() => View();
+    }
+}
