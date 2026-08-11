92f167608ed35e89c4d7592efa2288f4bff0c3eb
FlavMora <franciscomh98@gmail.com>
2026-08-10 23:58:10 -0600

    stilos

---
+++ EscuelaCopalchi.UI/Controllers/GestionAcademicaController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+
+namespace EscuelaCopalchi.UI.Controllers
+{
+    public class GestionAcademicaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aulas()
+        {
+            return View();
+        }
+
+        public ActionResult Asignaciones()
+        {
+            return View();
+        }
+
+        public ActionResult Distribucion()
+        {
+            return View();
+        }
+
+        public ActionResult Docentes()
+        {
+            return View();
+        }
+
+        public ActionResult MiGrupo()
+        {
+            return View();
+        }
+
+        public ActionResult Reportes()
+        {
+            return View();
+        }
+
+        public ActionResult ImportarExcel()
+        {
+            return View();
+        }
+    }
+}
---
+++ EscuelaCopalchi.UI/Controllers/RolesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace EscuelaCopalchi.UI.Controllers
+{
+    public class RolesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Editar(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Asignar(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Permisos(int? id)
+        {
+            return View();
+        }
+    }
+}
