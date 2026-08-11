d5692011c8d20d27b56aef3aeb46a8e913d748a4
FlavMora <franciscomh98@gmail.com>
2026-08-10 22:21:28 -0600

    GestionNotas

---
+++ EscuelaCopalchi.UI/Controllers/GestionNotasController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class GestionNotasController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        public ActionResult ImportarExcel()
+        {
+            return View();
+        }
+
+        public ActionResult Editar(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Historial()
+        {
+            return View();
+        }
+    }
+}
