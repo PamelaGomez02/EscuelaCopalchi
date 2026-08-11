69bd1dd9f2eeb1cd3fc9733b1bcc80ad4a829336
FlavMora <franciscomh98@gmail.com>
2026-08-10 21:49:10 -0600

    PlanesEstudio

---
+++ EscuelaCopalchi.UI/Controllers/PlanesEstudioController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class PlanesEstudioController : Controller
+    {
+        // GET: PlanesEstudio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Detalle(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Editar(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Revisar(int? id)
+        {
+            return View();
+        }
+    }
+}
